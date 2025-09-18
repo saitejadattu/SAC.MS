d38ac0100ec6027f28fc8b930fba1d4baa523c2c
saitejadattu <bodasaiteja@gmail.com>
2025-09-18 22:50:35 +0530

    added departement apis and auth login api

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -2,16 +2,17 @@ const router = require("express").Router();
 const supabase = require("../supaBaseClient");
 const bcrypt = require("bcrypt");
 router.post("/register", async (req, res) => {
-  const { email, username, password, role } = req.body;
+  const { email, full_name, password, role_id , department_id} = req.body;
+  console.log(req.body);
   try {
-    if (!email || !username || !password || !role) {
+    if (!email || !full_name || !password || !role_id || !department_id) {
       return res.status(400).json({ msg: "All fields are required" });
     }
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const password_hash = await bcrypt.hash(password, 10);
     // console.log(hashedPassword) // Implement password hashing here
     const { data, error } = await supabase
       .from("users")
-      .insert([{ email, username, hashedPassword, role }]);
+      .insert([{ email, full_name, password_hash, role_id , department_id}]);
     if (error) {
       throw error;
     }
@@ -24,8 +25,18 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.get("/login", async (req, res) => {
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  isUser = await supabase
+    .from("users")
+    .select("*")
+    .eq("email", email)
+    .single();
   
+  if (isUser.error) {
+    return res.status(400).json({ msg: "Invalid credentials" });
+  }
+  console.log(isUser)
   res.status(200).json({ msg: "login" });
 });
 
--- Routes/Dep.js
+++ Routes/Dep.js
@@ -1,24 +1,42 @@
-const router = require('express').Router();
-const supabase = require('../supaBaseClient');
+const router = require("express").Router();
+const supabase = require("../supaBaseClient");
 
 
-router.post('/add-department', async (req, res) => {
-    const { name, description } = req.body;
-    try {
-        if (!name || !description) {
-            return res.status(400).json({ msg: 'All fields are required' });
-        }
-        const { data, error } = await supabase
-            .from('departments')
-            .insert([{ name, description }]);
-        if (error) {
-            throw error;
-        }
-        return res.status(201).json({ msg: 'Department added successfully', data: data });
-    } catch (error) {
-        console.error('Error adding department:', error.message);
-        return res.status(500).json({ msg: 'Internal Server Error' });
+router.get("/all", async (req, res) => {
+  try {
+    const { data, error } = await supabase
+        .from("departments")
+        .select("*");
+    if (error) {
+      throw error;
     }
+    return res.status(200).json({ msg: "Departments fetched successfully", data: data });
+  } catch (error) {
+    console.error("Error fetching departments:", error.message);
+    return res.status(500).json({ msg: "Internal Server Error" });
+  } 
 });
+
+router.post("/add", async (req, res) => {
+  const { name } = req.body;
+  try {
+    if (!name) {
+      return res.status(400).json({ msg: "All fields are required" });
+    }
+    const { data, error } = await supabase
+      .from("departments")
+      .insert([{ name }]);
+    if (error) {
+      throw error;
+    }
+    return res
+      .status(201)
+      .json({ msg: "Department added successfully", data: data });
+  } catch (error) {
+    console.error("Error adding department:", error.message);
+    return res.status(500).json({ msg: "Internal Server Error" });
+  }
+});
+
 
 module.exports = router;
